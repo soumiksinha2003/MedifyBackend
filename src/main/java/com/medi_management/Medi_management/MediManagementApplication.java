aa14bfc920504dca3b01529fee86215d1a2c49f6
soumiksinha2003 <soumiksinha26102003@gmail.com>
2025-07-13 22:46:40 +0530

    Initial commit

---
+++ src/main/java/com/medi_management/Medi_management/MediManagementApplication.java
@@ -0,0 +1,13 @@
+package com.medi_management.Medi_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medi_management/Medi_management/MediManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi_management.Medi_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
